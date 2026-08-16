08a008d97dd287e348e0e2308aeb4f725323bc3e
shiriufc-bit <shiriufc@gmail.com>
2026-08-16 08:31:53 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// ══ FitQuest Service Worker v68 ══
+// ══ FitQuest Service Worker v70 ══
 // Gerado em: 09/08/2026 
-const CACHE_NAME = 'fitquest-v150';
+const CACHE_NAME = 'fitquest-v152';
 
 const ASSETS = [
   '/fitquest/',
